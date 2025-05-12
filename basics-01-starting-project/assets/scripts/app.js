aa2cf1803462c530fcda1d45c76d3dc398c2d39d
Vedanshi-Sadhu <vedanshisadhu11@gmail.com>
2025-05-12 14:08:33 -0400

    updated repo

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,7 @@
-alert('Hello!!!');
+// variables, operators, overwrite
+
+let currentResult = 0;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+let calculatonDescription = '(' + currentResult + '+ 10 ) * 3/2 -1';
+
+outputResult(currentResult, calculatonDescription);
