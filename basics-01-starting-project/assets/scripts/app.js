2b1366d93d48b6f4505150094c8a1e74a8c467d9
Vedanshi-Sadhu <vedanshisadhu11@gmail.com>
2025-07-16 20:58:39 -0400

    updated app.js and vendor.js using arrays and objects

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,76 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function add(
-    const calcDescription = `${currentResult} + ${userInput.value}`;
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult);
-)
+function getUserNumberInput() {
+    const enteredValue = parseInt(userInput.value);
+    if (isNaN(enteredValue)) {
+        alert("Please enter a valid number, babe!");
+        return 0; // Or handle this better
+    }
+    return enteredValue;
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+     outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+    operationIdentifier, 
+    prevResult, 
+    operationNumber, 
+    newResult
+){const logEntry = {
+        operation: operationIdentifier, 
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    //console.log(logEntry.operation) instead of 'operation: 'ADD', '
+    console.log(logEntries);
+
+}
+
+function add() {
+      console.log("Add button clicked!");
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+    userInput.value = '';
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+    userInput.value = '';
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+     writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+    userInput.value = '';
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+     writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+    userInput.value = '';
+}
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
